9b2218734c97384b06c67e6938455da5fd0147c4
vadim <115945880+Vadim-Shipnevsky@users.noreply.github.com>
2023-12-26 23:03:20 +0300

    feat: new script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +14,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
+  function onTelegramAuth(user : any) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
   return (
     <html lang="en" >
+     <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="authorizationMy" data-size="large" data-auth-url="https://authorization-five.vercel.app/" data-request-access="write"></script>
       <body className={inter.className}>
-      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="wdPororoBot" data-size="large" data-auth-url="https://authorization-five.vercel.app/" data-request-access="write"></script>
         {children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+       
+      <Link href={'https://telegram.org/js/telegram-widget.js?22'} >
+        telegram
+      </Link>
     </main>
   )
 }
